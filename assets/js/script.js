ab9cf7407932e5c516f1235a9319a2543a13db8d
Shirly <shirlyd@axioned.com>
2021-08-12 23:27:35 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
+var tab = document.querySelector('.tab');
+for (var j = 0; j < tab.length; j++) {
+    tab.addEventListener('click', city())
+}
+
 function city(e, nameOfCity) {
     var i;
-    var cityName = document.querySelectorAll('.city-name');
+    var cityName = document.querySelector('.city-name');
     for (i = 0; i < cityName.length; i++) {
-        cityName[i].className = 'none';
+        cityName[i].className = "none";
     }
-    var tab = document.querySelectorAll('.tab');
     for (i = 0; i < tab.length; i++) {
         tab[i].className = tab[i].className.replace(" active", "");
     }
+    document.querySelector(nameOfCity).className = "block";
+    e.currentTarget.className += " active"; 
 }
-
-document.querySelectorAll('.city-name').className = "block";
-e.currentTarget.className += " active";
-
-
-
 
 
 
