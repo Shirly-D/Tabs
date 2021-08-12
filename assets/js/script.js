36e234b769dd2b05f07fbb6fd721f61ec9817e25
Shirly <shirlyd@axioned.com>
2021-08-13 00:08:51 +0530

    js data attribute function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
-var tab = document.querySelector('.tab');
+var tab = document.querySelectorAll('.tab');
 for (var j = 0; j < tab.length; j++) {
-    tab.addEventListener('click', city())
+        tab[j].addEventListener('click', function(e){
+            if (this == e.target) {
+           var cityName = e.target.getAttribute("data-city");
+           city(e, cityName);
+           console.log(cityName);
+        }
+    });
+
 }
 
 function city(e, nameOfCity) {
     var i;
-    var cityName = document.querySelector('.city-name');
+    var cityName = document.querySelectorAll('.city-name');
     for (i = 0; i < cityName.length; i++) {
         cityName[i].className = "none";
     }
     for (i = 0; i < tab.length; i++) {
         tab[i].className = tab[i].className.replace(" active", "");
     }
-    document.querySelector(nameOfCity).className = "block";
+    document.querySelectorAll(nameOfCity).className = "block";
     e.currentTarget.className += " active"; 
 }
 
