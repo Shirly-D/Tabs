eb39c03dd2ea681dbac431e5433353c65149925c
Shirly <shirlyd@axioned.com>
2021-08-12 21:00:10 +0530

    js function commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-function city(e, name) {
-    
+function city(e, nameOfCity) {
+    var i;
+    var cityName = document.querySelectorAll('.city-name');
+    for (i = 0; i < cityName.length; i++) {
+        cityName[i].className = 'none';
+    }
+    var tab = document.querySelectorAll('.tab');
+    for (i = 0; i < tab.length; i++) {
+        tab[i].className = tab[i].className.replace(" active", "");
+    }
 }
 
-
-
+document.querySelectorAll('.city-name').className = "block";
+e.currentTarget.className += " active";
 
 
 
